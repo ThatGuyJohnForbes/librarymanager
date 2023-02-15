28e48969a939f2791529312a25c989dea45b680e
ThatGuyJohnForbes <thatbotjohn@gmail.com>
2023-02-15 16:14:55 +0000

    Start using TMDb and ffmpeg
    The current state of the script will expect all of the media library directories to be a directory above the librarymanager directory, and have each directory named the shows name.
    
    It currently only does TV shows and only the first season, but this is big progress!

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # librarymanager
 A Python script designed to automatically rename and edit the metadata of large media libraries, allowing for easier management of your movies and tv shows
+
+## Current limitations
+* only does tv shows
+* Expects media files to be in folders named the correct TV show's name
+* Doesn't remove the old media after renaming and changing the metadata
+* expects all media folders to be in the directory above the librarymanager directory 
+* Doesn't support nested directories
+* Doesn't support any seasons other than the first of any TV show
--- librarymanager.py
+++ librarymanager.py
@@ -2,15 +2,35 @@
 
 import os
 import asyncio
-import tmdb
+from decouple import config
+from tmdbv3api import TMDb, Movie, TV, Season
+import math
+
 
 def check_dir(path):
     files = os.listdir(path)
-    for f in files:
-        print(f)
+    print(path)
+    tmdb = TMDb()
+    tmdb.api_key = config('API_KEY')
+    tv = TV()
+    show = tv.search(path)
+    if not show:
+        return
+    season_number = 1
+    show_season = Season().details(show[0].id, season_number)
+    order_of_mag_of_episode_count = math.ceil(math.log(len(files),10)) #this allows us to pad the episode number with the correct amount of leading zeros
+    for x in range(0,len(files)):
+        episode = show_season.episodes[x]
+        print(files[x])
+        newfilename = f"({show[0].name}) [S{season_number}E{str(episode.episode_number).zfill(order_of_mag_of_episode_count)}] {episode.name}"
+        print(newfilename)
+        ffmpeg_command = f'ffmpeg -loglevel quiet -i "{path}/{files[x]}" -metadata title="{episode.name}" -metadata show="{show[0].name}" -metadata episode_id={episode.episode_number} -c copy -map 0 "{path}/{newfilename}.mkv"'
+        print(f"Running: {ffmpeg_command}")
+        os.system(ffmpeg_command)
 
 if __name__ == "__main__":
-    dirs = [x[0] for x in os.walk('.') if x[0] != '.' and x[0] != ".."]
+    os.chdir('..')
+    dirs = [x[0] for x in os.walk('.') if x[0] != '.' and x[0] != '..' and "librarymanager" not in x[0]]
     for dir in dirs:
         check_dir(dir)
 
